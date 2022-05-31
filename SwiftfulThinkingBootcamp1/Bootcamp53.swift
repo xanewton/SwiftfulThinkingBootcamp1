78e50acfd2e80d7ed43805765f48a60246089a65
Angel Newton <xanewton@gmail.com>
2022-05-31 10:22:09 -0400

    Add file

---
+++ SwiftfulThinkingBootcamp1/Bootcamp53.swift
@@ -0,0 +1,20 @@
+//
+//  Bootcamp53.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by newtan on 2022-05-31.
+//
+
+import SwiftUI
+
+struct Bootcamp53: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Bootcamp53_Previews: PreviewProvider {
+    static var previews: some View {
+        Bootcamp53()
+    }
+}
