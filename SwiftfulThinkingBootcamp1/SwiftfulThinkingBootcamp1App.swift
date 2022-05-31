09e6f20710246c8e56e1c962f4c09245ab795cc5
Angel Newton <xanewton@gmail.com>
2022-05-31 08:49:42 -0400

    Initial Commit

---
+++ SwiftfulThinkingBootcamp1/SwiftfulThinkingBootcamp1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcamp1App.swift
+//  SwiftfulThinkingBootcamp1
+//
+//  Created by newtan on 2022-05-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcamp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
